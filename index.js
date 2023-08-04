1fc9e575163be4aeda76a29d501f1e21b6787bb7
Alkın Alkan <83280716+alkinalkan@users.noreply.github.com>
2023-08-04 21:43:53 +0300

    Comment Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-// Event listener for keypress event
+// This code sets up an event listener to detect key presses on the webpage.
 document.addEventListener("keypress", function(event) {
-  // Call the makeSound function with the pressed key
+  // When a key is pressed, the makeSound function is called with the pressed key.
   makeSound(event.key);
   
-  // Call the buttonAnimation function with the pressed key
+  // Similarly, the buttonAnimation function is called with the pressed key
+  // to animate the associated button.
   buttonAnimation(event.key);
 });
 
-// Function to play the corresponding sound based on the key
+// This function plays a sound based on the provided key.
 function makeSound(key) {
-  // Create the path to the sound file
+  // Construct the path to the sound file using the key.
   var pathToSound = "sounds/" + key + ".mp3";
   
-  // Create a new Audio object with the sound file
+  // Create a new Audio object with the sound file path.
   var song = new Audio(pathToSound);
   
-  // Play the sound
+  // Play the sound using the play() method of the Audio object.
   song.play();
 }
 
-// Function to animate the button associated with the pressed key
+// This function animates the button associated with the pressed key.
 function buttonAnimation(currentKey) {
-  // Find the button element with the class equal to the pressed key
+  // Find the button element that has a class matching the pressed key.
   var activeButton = document.querySelector("." + currentKey);
   
-  // Add the "pressed" class to the button for animation
+  // Add the "pressed" class to the button element to trigger animation.
   activeButton.classList.add("pressed");
   
-  // Remove the "pressed" class after 100 milliseconds to revert the animation
+  // Remove the "pressed" class after a delay of 100 milliseconds
+  // to revert the animation effect.
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
